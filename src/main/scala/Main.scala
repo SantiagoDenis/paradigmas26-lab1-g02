672f3a75335725c2fd6667f0e8179a6e3212cd79
Santiago Denis <santidenis.sd@gmail.com>
2026-03-18 11:06:38 -0300

    feat: get subscriptions from local json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,12 @@
+import Types.Subscription
+
 object Main {
+
   def main(args: Array[String]): Unit = {
-    val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    val subsList: List[Subscription] = Formatters.getSubscriptions("subscriptions.json")
 
-    val allPosts: List[(String, String)] = subscriptions.map { url =>
-      println(s"Fetching posts from: $url")
-      val posts = FileIO.downloadFeed(url)
-      (url, posts)
-    }
-
-    val output = allPosts
-      .map { case (url, posts) => Formatters.formatSubscription(url, posts) }
-      .mkString("\n")
-
-    println(output)
+    println(subsList.mkString("\n"))
   }
+
 }
